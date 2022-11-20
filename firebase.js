4b71756a0b902ef97ecd309bf668f29ee561b8ce
demonking15543 <demonking15543@gmail.com>
2022-11-20 17:02:26 +0530

    NEXT firebase settings added

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
 // import firebase from 'firebase';
-import { getFirestore, collection, Timestamp, doc, addDoc, updateDoc, getDoc, setDoc, query, where, getDocs  } from 'firebase/firestore';
+import { getFirestore, collection, Timestamp, doc, addDoc, updateDoc, getDoc, setDoc, query, where, getDocs, orderBy  } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app'
-const config = require("./firebase.config.json")
-const firebaseConfig = config
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
+}
 
   const app = initializeApp(firebaseConfig);
  export const db = getFirestore(app)
