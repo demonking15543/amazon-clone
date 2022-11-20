fe893ddab182a6c32baab883472ef669db5888e5
demonking15543 <demonking15543@gmail.com>
2022-11-20 15:23:43 +0530

    firebase config file dump into .gitignore

--- firebase.js
+++ firebase.js
@@ -1,14 +1,8 @@
 // import firebase from 'firebase';
 import { getFirestore, collection, Timestamp, doc, addDoc, updateDoc, getDoc, setDoc, query, where, getDocs  } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app'
-const firebaseConfig = {
-    apiKey: "AIzaSyBt5sssarlWn7ZtOdCJZwSAvIKPFKJkaQA",
-    authDomain: "amzn-1b318.firebaseapp.com",
-    projectId: "amzn-1b318",
-    storageBucket: "amzn-1b318.appspot.com",
-    messagingSenderId: "102506291219",
-    appId: "1:102506291219:web:5e5b1fdacffd65051e5fe6"
-  };
+const config = require("./firebase.config.json")
+const firebaseConfig = config
 
   const app = initializeApp(firebaseConfig);
  export const db = getFirestore(app)
